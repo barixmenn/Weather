045efad99f21d105bd9fa7a70e03b7284c93eb70
baris dilekci <bdilekci200@gmail.com>
2023-06-25 16:33:38 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Baris on 25.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
